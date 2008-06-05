ef56bd465ab091b4445a6cdcfe89cd83af4b04b9
jstevens <jstevens@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-06-05 19:21:06 +0000

    refs #613  * fixing compat_zenoss_version
    git-svn-id: http://dev.zenoss.com/svnint/trunk/enterprise/zenpacks/ZenPacks.zenoss.ZenMailTx@1819 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ AUTHOR = 'Zenoss'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
 INSTALL_REQUIRES = []
-COMPAT_ZENOSS_VERS = '2.2'
+COMPAT_ZENOSS_VERS = '>= 2.2'
 PREV_ZENPACK_NAME = 'ZenMailTx'
 # STOP_REPLACEMENTS
 ################################
