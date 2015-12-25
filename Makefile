7f96397ea6ee8ca4891d128610d4daf4a9f47303
ssbunyk <tbunyk@softserveinc.com>
2015-12-25 15:38:41 +0200

    add datasource

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 egg:
 	python setup.py bdist_egg
+
+test:
+	runtests -v ZenPacks.zenoss.ZenMailTx
--- ZenPacks/zenoss/ZenMailTx/datasources/MailTxPythonDataSource.py
+++ ZenPacks/zenoss/ZenMailTx/datasources/MailTxPythonDataSource.py
@@ -1,10 +1,10 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2015, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
@@ -15,7 +15,9 @@ Part of ZenMailTx zenpack.
 
 from zope.component import adapts
 from zope.interface import implements
- 
+from zope.schema.vocabulary import SimpleVocabulary
+from twisted.internet import defer
+
 from Products.Zuul.form import schema
 from Products.Zuul.infos import ProxyProperty
 from Products.Zuul.infos.template import RRDDataSourceInfo
@@ -52,7 +54,7 @@ class MailTxPythonDataSource(PythonDataSource):
     cycleTime = 300
     timeout = 300
     messageBody = '''This message is used by the Zenoss monitoring system to gauge e-mail delivery transaction time. Please ignore this message.'''
-    
+
     eventClass = '/App/Email/Loop'
 
     _properties = PythonDataSource._properties + (
@@ -74,7 +76,7 @@ class MailTxPythonDataSource(PythonDataSource):
         {'id':'messageBody', 'type':'string', 'mode':'w'},
     )
 
-        
+
     def __init__(self, id, title=None, buildRelations=True):
         super(MailTxPythonDataSource, self).__init__(id, title, buildRelations)
 
@@ -89,21 +91,63 @@ class MailTxPythonDataSource(PythonDataSource):
 class IMailTxPythonDataSourceInfo(IRRDDataSourceInfo):
     """Interface that creates the web form for this data source type."""
 
-    smtpHost = schema.TextLine(title=_t('SMTP Host'))
-    smtpPort = schema.TextLine(title=_t('SMTP Port'))
-    # TODO:
-        {'id':'smtpPort', 'type':'int', 'mode':'w'},
-        {'id':'smtpUsername', 'type':'string', 'mode':'w'},
-        {'id':'smtpPassword', 'type':'string', 'mode':'w'},
-        {'id':'toAddress', 'type':'string', 'mode':'w'},
-        {'id':'fromAddress', 'type':'string', 'mode':'w'},
-        {'id':'smtpAuth', 'type':'string', 'mode':'w'},
-        {'id':'popHost', 'type':'string', 'mode':'w'},
-        {'id':'popPort', 'type':'string', 'mode':'w'},
-        {'id':'popUsername', 'type':'string', 'mode':'w'},
-        {'id':'popPassword', 'type':'string', 'mode':'w'},
-        {'id':'popAuth', 'type':'string', 'mode':'w'},
-        {'id':'popAllowInsecureLogin', 'type':'boolean', 'mode':'w'},
-        {'id':'cycleTime', 'type':'int', 'mode':'w'},
-        {'id':'timeout', 'type':'int', 'mode':'w'},
-        {'id':'messageBody', 'type':'string', 'mode':'w'},
+    toAddress = schema.TextLine(title=_t('To address'), group=_t('Email'))
+    fromAddress = schema.TextLine(title=_t('From address'), group=_t('Email'))
+    messageBody = schema.Text(title=_t('Message body'), group=_t('Email'))
+
+    cycleTime = schema.Int(title=_t('Cycle time'))
+    timeout = schema.Int(title=_t('Timeout'))
+
+    smtpHost = schema.TextLine(title=_t('SMTP host'), group=_t('SMTP'))
+    smtpPort = schema.TextLine(title=_t('SMTP port'), group=_t('SMTP'))
+    smtpUsername = schema.TextLine(title=_t('SMTP username'), group=_t('SMTP'))
+    smtpPassword = schema.Password(title=_t('SMTP password'), group=_t('SMTP'))
+    smtpAuth = schema.Choice(
+        title=_t('SMTP auth'),
+        group=_t('SMTP'),
+        vocabulary=SimpleVocabulary.fromValues(MailTxPythonDataSource.AuthModes)
+    )
+
+    popHost = schema.TextLine(title=_t('POP host'), group=_t('POP'))
+    popPort = schema.TextLine(title=_t('POP port'), group=_t('POP'))
+    popUsername = schema.TextLine(title=_t('POP username'), group=_t('POP'))
+    popPassword = schema.Password(title=_t('POP password'), group=_t('POP'))
+    popAuth = schema.Choice(
+        title=_t('POP auth'),
+        group=_t('POP'),
+        vocabulary=SimpleVocabulary.fromValues(MailTxPythonDataSource.AuthModes)
+    )
+    popAllowInsecureLogin = schema.Bool(title=_t('Allow insecure login'), group=_t('POP'))
+
+
+
+class MailTxPythonDataSourceInfo(RRDDataSourceInfo):
+    implements(IMailTxPythonDataSourceInfo)
+    adapts(MailTxPythonDataSource)
+
+    testable = False
+
+    smtpHost = ProxyProperty('smtpHost')
+    smtpPort = ProxyProperty('smtpPort')
+    smtpUsername = ProxyProperty('smtpUsername')
+    smtpPassword = ProxyProperty('smtpPassword')
+    toAddress = ProxyProperty('toAddress')
+    fromAddress = ProxyProperty('fromAddress')
+    smtpAuth = ProxyProperty('smtpAuth')
+    popHost = ProxyProperty('popHost')
+    popPort = ProxyProperty('popPort')
+    popUsername = ProxyProperty('popUsername')
+    popPassword = ProxyProperty('popPassword')
+    popAuth = ProxyProperty('popAuth')
+    popAllowInsecureLogin = ProxyProperty('popAllowInsecureLogin')
+    cycleTime = ProxyProperty('cycleTime')
+    timeout = ProxyProperty('timeout')
+    messageBody = ProxyProperty('messageBody')
+
+
+class MailTxPythonDataSourcePlugin(PythonDataSourcePlugin):
+    @defer.inlineCallbacks
+    def collect(self, config):
+        results = self.new_data()
+        yield
+        defer.returnValue(results)
